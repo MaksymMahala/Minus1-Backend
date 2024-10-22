0fedb38ddd20faada199e513098265f2db84fe2a
Maksym Mahala <maksym.mahala.kn.2021@lpnu.ua>
2024-10-22 11:47:05 +0300

    no message

--- back-end/REST/main-rest-methods.js
+++ back-end/REST/main-rest-methods.js
@@ -10,7 +10,7 @@ const login = require("./routes/login");
 const lastPrices = require("./routes/last-prices");
 const mongoose = require("mongoose");
 
-const PORT = 5500;
+const PORT = 3000;
 
 const app = express();
 
@@ -31,6 +31,12 @@ app.use(express.json());
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
+});
+
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to the Cryptocurrency API. Use /api/cryptocurrencies to get data."
+  );
 });
 
 app.get("/api/cryptocurrencies", async (req, res) => {
